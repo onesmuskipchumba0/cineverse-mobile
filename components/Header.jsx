2d17db0bf1ada72ed39a6042babb54878f94963e
OnesmusBett <onesmuskipchumba5@gmail.com>
2024-11-22 10:26:21 +0300

    Add header styles

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,14 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { FilmIcon } from 'react-native-heroicons/solid'
 
 const Header = () => {
   return (
     <View className="flex-row justify-between items-center p-5">
-      <Text className="text-2xl font-bold">MovieBox</Text>
+      <View className="flex-row items-center mt-5 space-x-2">
+        <FilmIcon size={28} color="#3b82f6" />
+        <Text className="text-2xl font-bold text-white">Cineverse</Text>
+      </View>
     </View>
   )
 }
